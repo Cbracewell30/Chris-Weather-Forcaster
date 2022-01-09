2d20ecfa285121ffb79b9dba714027f11e106005
Chris <chrisbracewell35@gmail.com>
2022-01-08 22:49:47 -0500

    addding current temp

--- assets/java/script.js
+++ assets/java/script.js
@@ -7,7 +7,7 @@ $("#subBtn").on("click", function () {
 });
 
 function getLocation(cityName) {
-    var apiURL =`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKEy}`;
+    var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKEy}`;
     $.ajax({
         type: 'GET',
         url: apiURL,
@@ -16,25 +16,38 @@ function getLocation(cityName) {
             console.log(apiData)
             var lat = apiData.coord.lat;
             var lon = apiData.coord.lon;
-            oneCallApi(lon,lat);
-        },
+            oneCallApi(lon, lat);
+        }, 
         error: function (err) {
             console.log("Error in getting API Data", err)
         }
-    })
+    });
+
 };
 
-function oneCallApi(lon,lat){
-var oneCall = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKEy}`
-$.ajax({
-    type: 'GET',
-    url: oneCall,
-    datatype: 'JSON',
-    success: function (apiData) {
-        console.log(apiData);
-    },
-    error: function (err) {
-        console.log("Error in getting oneCall API Data", err)
-    }
-})
-};
+        function oneCallApi(lon, lat) {
+        var oneCall = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKEy}&units=imperial`
+            $.ajax({
+                type: 'GET',
+                url: oneCall,
+                datatype: 'JSON',
+                success: function (apiData) {
+                    console.log(apiData);
+                    $("#current").html(`<h4 class="font-weight-bold">${cityNAme}
+                    <p>temp:${apiData.daily[0].temp.day}</p>
+                    <img src ="http://openweathermap.org/img/wn/${apiData.daily[0].weather[0].icon}@2x.png" />
+                    <p>Humidity: ${}</p>
+                    <p>WindSpeed: ${}</p>
+                    `)
+
+                    for(let i=1;i<6;i++){
+                        $(`#day-${i}`).html(``)
+                    }
+
+
+                },
+                error: function (err) {
+                    console.log("Error in getting oneCall API Data", err)
+                }
+           })
+        };
