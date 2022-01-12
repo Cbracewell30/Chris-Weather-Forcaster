ddf97592ca5f9ea935ee2c846ec2f39ab808fe47
Chris <chrisbracewell35@gmail.com>
2022-01-12 18:12:53 -0500

    adding changes to CSS and Java

--- assets/java/script.js
+++ assets/java/script.js
@@ -27,7 +27,7 @@ function getLocation(cityName) {
 
 function oneCallApi(lon, lat, cityName) {
     
-    var oneCall = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&exclude=minutely,hourly,alerts&units=imperial`;
+    var oneCall = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`;
     $.ajax({
         type: 'GET',
         url: oneCall,
@@ -41,12 +41,14 @@ function oneCallApi(lon, lat, cityName) {
                     <p>Wind Speed: ${apiData.current.wind_speed}</p>
                     `)
 
-            for (let i = 1; i < 6; i++) {
-                $(`#day-${i}`).html(`<h4 class="font-weight-bold">${cityName}
-                <p>Temp: ${apiData.daily[i].temp.day}°F</p>
+            for (let i = 1; i < 7; i++) {
+                $(`#day-${i}`).html(`
+                <p>Temp: ${apiData.daily[i].temp.day}°F</p> 
                 <p>Temp Evening: ${apiData.daily[i].temp.eve}°F</p>
                 <p>Humidity: ${apiData.daily[i].humidity}</p>
                 <p>Wind Speed: ${apiData.daily[i].wind_speed}</p>
+                `)
+                $(`#city-${i}`).html(`<h4 class="font-weight-bold"> Day ${i}: ${cityName}
                 `)
 
             }
