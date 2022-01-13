0238265ca6abc6b2f8083394bd1d780a3b2cb706
Chris <chrisbracewell35@gmail.com>
2022-01-13 00:30:28 -0500

    adding css classes

--- assets/java/script.js
+++ assets/java/script.js
@@ -35,17 +35,19 @@ function oneCallApi(lon, lat, cityName) {
         success: function (apiData) {
             console.log(apiData);
             $("#current").html(`<h4 class="font-weight-bold">${cityName}
+            
                     <p>Temp: ${apiData.current.temp}°F</p>
-                   
                     <p>Humidity: ${apiData.current.humidity}</p>
                     <p>Wind Speed: ${apiData.current.wind_speed}</p>
+                    
                     `)
 
             for (let i = 1; i < 7; i++) {
                 $(`#day-${i}`).html(`
                 <p>Temp: ${apiData.daily[i].temp.day}°F</p> 
-                <p>Temp Evening: ${apiData.daily[i].temp.eve}°F</p>
+                <br>
                 <p>Humidity: ${apiData.daily[i].humidity}</p>
+                <br>
                 <p>Wind Speed: ${apiData.daily[i].wind_speed}</p>
                 `)
                 $(`#city-${i}`).html(`<h4 class="font-weight-bold"> Day ${i}: ${cityName}
